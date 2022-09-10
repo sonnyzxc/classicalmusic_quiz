b3150b1d4dcc632e104858a9509c0c212386d563
Sonny Ku <sonnyk@macbook9472.local>
2022-09-10 13:19:31 +0900

    first commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+from urllib import request
+import discord, requests, json, asyncio
+
+client = discord.Client() #build connection to discord
+token = 'MTAwNTU2MDQwMDY1OTYxMTczMA.GrECXi.K6bOxk-CApx-AtAARwA5pI_31gtWa6HyaqF57c'
+
+def get_question():
+    q = ''
+    id = 1
+    a = 0
+
+    response = requests.get("http://127.0.0.1:8000/api/random/")
+    json_data = json.loads(response.text)
+    q += json_data[0]['title'] + "\n"
+
+    for x in json_data[0]['answer']:
+        q += str(id) + ". " + x['answer'] + "\n"
+
+        if x['is_correct'] == True:
+            a = id
+
+        id += 1
+
+    return(q,a)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return #ignore messages from self (bot)
+
+    if message.content.startswith('$q'):
+        qs, ans = get_question()
+        await message.channel.send(qs)
+
+        def check(m):
+            return m.author == message.author and m.content.isdigit()
+        
+        try:
+            guess = await client.wait_for('message', check=check, timeout=5.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send('Time\'s up!')
+
+        if int(guess.content) == ans:
+            await message.channel.send('Correct!')
+        else:
+            await message.channel.send('Wrong!')
+
+client.run(token)
---
+++ quizbot/quiz/admin.py
@@ -0,0 +1,33 @@
+from dataclasses import fields
+from django.contrib import admin
+from . import models
+
+class AnswerInlineModel(admin.TabularInline):
+    model = models.Answer
+    fields = [
+        'answer',
+        'is_correct',
+        ]
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        'answer',
+        'is_correct',
+        'question'
+        ]
+
+@admin.register(models.Question) 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'points',
+        'difficulty'
+        ]
+    list_display = [
+        'title',
+        'updated_at'
+        ] 
+    inlines = [
+        AnswerInlineModel,
+        ]
